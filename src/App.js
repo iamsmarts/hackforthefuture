42ccbae634203a0fb576c3e96ced0d37a438f4c6
Rudy Quevedo <iamsmarts@DESKTOP-0AGC3O4.localdomain>
2020-07-05 16:07:13 -0700

    updated email form

--- src/App.js
+++ src/App.js
@@ -36,9 +36,31 @@ function App() {
         <div className="row">
           <div className="content_email-collection col align-self-center">
             <p>Stay Up to Date on this Event</p>
-            <form>
-              <input type="email" placeholder="Please Enter Email" />
-              <button type="submit">submit</button>
+            <form
+              action="https://hacktothefuture.us10.list-manage.com/subscribe/post?u=cf3f2e1ca214d8059b913759b&amp;id=11ef43cb28"
+              method="post"
+              id="mc-embedded-subscribe-form"
+              name="mc-embedded-subscribe-form"
+              className="validate"
+              target="_blank"
+              novalidate
+            >
+              <input
+                type="email"
+                // value=""
+                name="EMAIL"
+                className="required email"
+                id="mce-EMAIL"
+                placeholder="Please Enter Email"
+              />
+              <button
+                type="submit"
+                value="Subscribe"
+                name="subscribe"
+                id="mc-embedded-subscribe"
+              >
+                submit
+              </button>
             </form>
           </div>
         </div>
